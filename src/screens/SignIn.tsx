216019fb69eedef960301d134d53ab4849e9e81a
BrunoCaputo <brunopc.dev@gmail.com>
2025-05-12 19:39:54 -0300

    feat: Enable svg usage

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,6 @@
 import BackgroundImg from '@assets/background.png'
-import { Image, VStack } from '@gluestack-ui/themed'
+import Logo from '@assets/logo.svg'
+import { Center, Image, Text, VStack } from '@gluestack-ui/themed'
 
 export function SignIn() {
   return (
@@ -12,6 +13,14 @@ export function SignIn() {
         alt="Pessoas treinando"
         position="absolute"
       />
+
+      <Center my="$24">
+        <Logo />
+
+        <Text color="$gray100" fontSize="$sm">
+          Treine sua mente e seu corpo
+        </Text>
+      </Center>
     </VStack>
   )
 }
