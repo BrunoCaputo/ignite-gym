5c5a2dee290b30c7816bdc09c9b8ba3c04a10ce5
BrunoCaputo <brunopc.dev@gmail.com>
2025-05-15 23:19:03 -0300

    feat: Using lucide react icons

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,19 @@
-import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { Heading, HStack, Icon, Text, VStack } from '@gluestack-ui/themed'
+import { LogOut } from 'lucide-react-native'
+
 import { UserPhoto } from './UserPhoto'
 
 export function HomeHeader() {
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap='$4'>
-      <UserPhoto source={{ uri: 'https://github.com/BrunoCaputo.png'}} w='$16' h='$16' alt='Imagem do usuário' />
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto
+        source={{ uri: 'https://github.com/BrunoCaputo.png' }}
+        w="$16"
+        h="$16"
+        alt="Imagem do usuário"
+      />
 
-      <VStack>
+      <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">
           Olá
         </Text>
@@ -14,6 +21,8 @@ export function HomeHeader() {
           Bruno Caputo
         </Heading>
       </VStack>
+
+      <Icon as={LogOut} color="$gray200" size="xl" />
     </HStack>
   )
 }
