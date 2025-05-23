f499f523b14a5ed317594cdf138fbda86d613ff4
BrunoCaputo <brunopc.dev@gmail.com>
2025-05-23 13:55:07 -0300

    feat: Profile input fields

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,23 @@
 import { Input as GluestackInput, InputField } from '@gluestack-ui/themed'
 import { ComponentProps } from 'react'
 
-type InputProps = ComponentProps<typeof InputField>
+interface InputProps extends ComponentProps<typeof InputField> {
+  isReadOnly?: boolean
+}
 
-export function Input({ ...props }: InputProps) {
+export function Input({ isReadOnly = false, ...props }: InputProps) {
   return (
     <GluestackInput
-      bg="$gray700"
       h="$14"
-      px="$4"
       borderWidth="$0"
       borderRadius="$md"
       $focus={{ borderWidth: 1, borderColor: '$green500' }}
+      isReadOnly={isReadOnly}
+      opacity={isReadOnly ? 0.5 : 1}
     >
       <InputField
+        px="$4"
+        bg="$gray700"
         color="$white"
         fontFamily="$body"
         placeholderTextColor="$gray300"
