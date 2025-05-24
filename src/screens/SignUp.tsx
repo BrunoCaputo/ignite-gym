885344e73b64b177894f27fb434347bad74c8ae7
BrunoCaputo <brunopc.dev@gmail.com>
2025-05-24 13:13:56 -0300

    feat: Submit signup form

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -12,107 +12,132 @@ import {
 } from '@gluestack-ui/themed'
 import { useNavigation } from '@react-navigation/native'
 import { Controller, useForm } from 'react-hook-form'
+import { KeyboardAvoidingView, Platform } from 'react-native'
+
+interface FormDataProps {
+  name: string
+  email: string
+  password: string
+  password_confirm: string
+}
 
 export function SignUp() {
-  const { control } = useForm()
+  const { control, handleSubmit } = useForm<FormDataProps>()
   const navigation = useNavigation()
 
   function handleLogin() {
     navigation.goBack()
   }
 
-  function handleSignUp() {}
+  function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirm,
+  }: FormDataProps) {
+    console.log({ name, email, password, password_confirm })
+  }
 
   return (
-    <ScrollView
-      contentContainerStyle={{ flexGrow: 1 }}
-      showsVerticalScrollIndicator={false}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
     >
-      <VStack flex={1}>
-        <Image
-          w="$full"
-          h={624}
-          source={BackgroundImg}
-          defaultSource={BackgroundImg}
-          alt="Pessoas treinando"
-          position="absolute"
-        />
-
-        <VStack flex={1} px="$10" pb="$16">
-          <Center my="$24">
-            <Logo />
-
-            <Text color="$gray100" fontSize="$sm">
-              Treine sua mente e seu corpo
-            </Text>
-          </Center>
-
-          <Center flex={1} gap="$2">
-            <Heading color="$gray100">Crie sua conta</Heading>
-
-            <Controller
-              control={control}
-              name="name"
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  placeholder="Nome"
-                  onChangeText={onChange}
-                  value={value}
-                />
-              )}
-            />
-
-            <Controller
-              control={control}
-              name="email"
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  placeholder="E-mail"
-                  keyboardType="email-address"
-                  autoCapitalize="none"
-                  onChangeText={onChange}
-                  value={value}
-                />
-              )}
-            />
-
-            <Controller
-              control={control}
-              name="password"
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  placeholder="Senha"
-                  secureTextEntry
-                  onChangeText={onChange}
-                  value={value}
-                />
-              )}
-            />
-
-            <Controller
-              control={control}
-              name="password_confirm"
-              render={({ field: { onChange, value } }) => (
-                <Input
-                  placeholder="Confirme a Senha"
-                  secureTextEntry
-                  onChangeText={onChange}
-                  value={value}
-                />
-              )}
-            />
-
-            <Button title="Criar e acessar" onPress={handleSignUp} />
-          </Center>
-
-          <Button
-            title="Voltar para o login"
-            variant="outline"
-            mt="$12"
-            onPress={handleLogin}
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <VStack flex={1}>
+          <Image
+            w="$full"
+            h={624}
+            source={BackgroundImg}
+            defaultSource={BackgroundImg}
+            alt="Pessoas treinando"
+            position="absolute"
           />
+
+          <VStack flex={1} px="$10" pb="$16">
+            <Center my="$24">
+              <Logo />
+
+              <Text color="$gray100" fontSize="$sm">
+                Treine sua mente e seu corpo
+              </Text>
+            </Center>
+
+            <Center flex={1} gap="$2">
+              <Heading color="$gray100">Crie sua conta</Heading>
+
+              <Controller
+                control={control}
+                name="name"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Nome"
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="email"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="E-mail"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Senha"
+                    secureTextEntry
+                    onChangeText={onChange}
+                    value={value}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="password_confirm"
+                render={({ field: { onChange, value } }) => (
+                  <Input
+                    placeholder="Confirme a Senha"
+                    secureTextEntry
+                    onChangeText={onChange}
+                    value={value}
+                    onSubmitEditing={handleSubmit(handleSignUp)}
+                    returnKeyType="send"
+                  />
+                )}
+              />
+
+              <Button
+                title="Criar e acessar"
+                onPress={handleSubmit(handleSignUp)}
+              />
+            </Center>
+
+            <Button
+              title="Voltar para o login"
+              variant="outline"
+              mt="$12"
+              onPress={handleLogin}
+            />
+          </VStack>
         </VStack>
-      </VStack>
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 }
