ab0fdab3376d9fec7b81ccf07afbb934295bfaf6
BrunoCaputo <brunopc.dev@gmail.com>
2025-06-14 12:33:36 -0300

    feat: Get groups and exercises from API

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,29 +1,87 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ExerciseCard } from '@components/ExerciseCard'
 import { Group } from '@components/Group'
 import { HomeHeader } from '@components/HomeHeader'
-import { Heading, HStack, Text, VStack } from '@gluestack-ui/themed'
+import { ToastMessage } from '@components/ToastMessage'
+import { Heading, HStack, Text, useToast, VStack } from '@gluestack-ui/themed'
 import { useAuth } from '@hooks/useAuth'
-import { useNavigation } from '@react-navigation/native'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { AppNavigatorRoutesProps, AppRoutes } from '@routes/app.routes'
-import { useState } from 'react'
+import { api } from '@services/api'
+import { AppError } from '@utils/AppError'
+import { useCallback, useEffect, useState } from 'react'
 import { FlatList } from 'react-native'
 
 export function Home() {
-  const [exercises] = useState<string[]>([
-    'Puxada frontal',
-    'Remada curvada',
-    'Remada unilateral',
-    'Levantamento terra',
-  ])
-  const [groups] = useState<string[]>(['Costas', 'Bíceps', 'Tríceps', 'Ombro'])
+  const [exercises, setExercises] = useState<string[]>([])
+  const [groups, setGroups] = useState<string[]>([])
   const [groupSelected, setGroupSelected] = useState<string>('Costas')
 
   const { signOut, user } = useAuth()
+  const toast = useToast()
   const navigation = useNavigation<AppNavigatorRoutesProps>()
 
   function handleOpenExerciseDetails(params: AppRoutes['exercise']) {
     navigation.navigate('exercise', params)
   }
+
+  async function fetchGroups() {
+    try {
+      const response = await api.get('/groups')
+      setGroups(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar os grupos musculares'
+
+      toast.show({
+        placement: 'top',
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
+    }
+  }
+
+  async function fecthExercisesByGroup() {
+    try {
+      const response = await api.get(`/exercises/bygroup/${groupSelected}`)
+      setExercises(response.data)
+    } catch (error) {
+      const isAppError = error instanceof AppError
+      const title = isAppError
+        ? error.message
+        : 'Não foi possível carregar os exercícios'
+
+      toast.show({
+        placement: 'top',
+        render: ({ id }) => (
+          <ToastMessage
+            id={id}
+            action="error"
+            title={title}
+            onClose={() => toast.close(id)}
+          />
+        ),
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchGroups()
+  }, [])
+
+  useFocusEffect(
+    useCallback(() => {
+      fecthExercisesByGroup()
+    }, [groupSelected]),
+  )
 
   return (
     <VStack flex={1}>
